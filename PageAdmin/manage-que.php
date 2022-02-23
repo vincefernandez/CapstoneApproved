1867756c5dd097cf3d13925b504934e0e6e455bf
vincenfernandez <vincentfernandez727@gmail.com>
2022-02-23 21:08:07 +0800

    asda

--- PageAdmin/manage-que.php
+++ PageAdmin/manage-que.php
@@ -1,50 +1,114 @@
 
-<?php 
- include('../app/class.php');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+include_once('../app/class.php');
+
+session_start();
+$Employee_ID = $_SESSION['login'];
+if (isset($_SESSION['login'])) {
+    if ($_SESSION['login'] == 001) {
+        echo 'Hello ' . $_SESSION['login'];
+    } else {
+        header('location: ../errorPages/err1.php');
+    }
+} else {
+    header('location: ../errorPages/err1.php');
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php include('queque-Header.html'); ?>
 
+<body class="sb-nav-fixed">
+    <?php include('navbar.php');
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js" integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <title>asdasd</title>
-</head>
+    ?>
 
-<body>
-    <div class="container">
-        <canvas id="myChart">
+    <div id="layoutSidenav">
+        <?php include('sidebar.php');
 
-        </canvas>
+        ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Welcome Administrator</h1>
+
+                    <div>
+                        <canvas id="myChart" style="position: relative; height:40vh; width:80vw">
+
+                        </canvas>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script>
-        let myChart = document.getElementById('myChart').getContext('2d');
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="../assets/js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <!-- <script src="../assets/demo/chart-area-demo.js"></script> -->
+    <!-- <script src="../assets/demo/chart-bar-demo.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="../assets/js/datatables-simple-demo.js"></script>
+<script>
+    let myChart = document.getElementById('myChart').getContext('2d');
 
-        let massPopChart = new Chart(myChart, {
-            type: 'bar',
-            data:{
-                labels:['ALL Employee','CC CAMARIN','aw','aw','aw'],
-                datasets:[{
-                    label: 'population',
-                    data:[
-                        <?php $student->CountData();?>,
-                        30,
-                        40,
-                        50,
-                        40,
-                    ]
-                }],
-            },
-            options:{}
-        });
+    let massPopChart = new Chart(myChart, {
+    type: 'bar',
+    data: {
+    labels: ['ALL Employee', 'CC CAMARIN', 'aw', 'aw', 'aw'],
+    datasets: [{
+    label: 'Monthly Records',
+    data: [
+    <?php $student->CountData(); ?>,
+    100,
+    40,
+    50,
+    40,
+    ]
+    }],
+    },
+    options: {}
+    });
     </script>
-
 </body>
 
 </html>
