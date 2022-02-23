095c5d0413b59cc4d2c2714ddc86ab2f6b246374
vincefernandez <vincentfernandez727@gmail.com>
2022-02-23 13:16:16 +0800

    2/23/22

--- PageAdmin/AdminSidebar.php
+++ PageAdmin/AdminSidebar.php
@@ -80,15 +80,37 @@
                             </a>
                         </nav>
                     </div>
-                   <div class="sb-sidenav-menu-heading">Addons</div>
-                    <a class="nav-link" href="charts.html">
+                      <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#Queueing" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Queueing Seciton
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="Queueing" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                            <a class="nav-link" href="../queque/AllTransactions.php">
+                                <div class="sb-nav-link-icon"></div>
+                                All Transactions
+                            </a>
+
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"></div>
+                                Window List
+                            </a>
+                            <a class="nav-link" href="../queque/Transaction_List.php">
+                                <div class="sb-nav-link-icon"></div>
+                                Transaction List
+                            </a>
+                        </nav>
+                    </div>
+                   <!-- <div class="sb-sidenav-menu-heading">Addons</div> -->
+                    <!-- <a class="nav-link" href="charts.html">
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                         Charts
                     </a>
                     <a class="nav-link" href="tables.html">
                         <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                         Tables
-                    </a> 
+                    </a>  -->
                 </div>
             </div>
          <!-- <div class="sb-sidenav-footer h-10 w-10">
